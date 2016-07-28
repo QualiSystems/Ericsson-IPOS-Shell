e8d6bacdb690006aff431b41ac9826c22722d1ad
CoYe <costya.y@quali.com>
2016-07-28 15:55:16 -0700

    added new firmware operations handler

--- cloudshell/networking/ericsson/ipos/ericsson_ipos_configuration.py
+++ cloudshell/networking/ericsson/ipos/ericsson_ipos_configuration.py
@@ -2,6 +2,7 @@ import re
 
 from cloudshell.networking.ericsson.ericsson_configuration_operations import EricssonConfigurationOperations
 from cloudshell.networking.ericsson.ericsson_connectivity_operations import EricssonConnectivityOperations
+from cloudshell.networking.ericsson.ericsson_firmware_operations import EricssonFirmwareOperations
 from cloudshell.networking.ericsson.ericsson_send_command_operations import EricssonSendCommandOperations
 from cloudshell.networking.ericsson.ipos.autoload.ericsson_ipos_snmp_autoload import EricssonIPOSSNMPAutoload
 from cloudshell.shell.core.context_utils import get_decrypted_password_by_attribute_name_wrapper, \
@@ -18,9 +19,8 @@ def send_default_actions(session):
     :return:
     """
 
-    # print 'default actions'
     enter_enable_mode(session=session)
-    # session.hardware_expect('terminal length 0', ENABLE_PROMPT)
+    session.hardware_expect('terminal length 0', ENABLE_PROMPT)
     # session.hardware_expect('terminal no exec prompt timestamp', ENABLE_PROMPT)
     # session.hardware_expect(ENTER_CONFIG_MODE_PROMPT_COMMAND, CONFIG_MODE_PROMPT)
     # session.hardware_expect('no logging console', CONFIG_MODE_PROMPT)
@@ -50,7 +50,7 @@ def enter_enable_mode(session):
 
 CONNECTIVITY_OPERATIONS_CLASS = EricssonConnectivityOperations
 CONFIGURATION_OPERATIONS_CLASS = EricssonConfigurationOperations
-FIRMWARE_OPERATIONS_CLASS = EricssonConfigurationOperations
+FIRMWARE_OPERATIONS_CLASS = EricssonFirmwareOperations
 AUTOLOAD_OPERATIONS_CLASS = EricssonIPOSSNMPAutoload
 SEND_COMMAND_OPERATIONS_CLASS = EricssonSendCommandOperations
 
