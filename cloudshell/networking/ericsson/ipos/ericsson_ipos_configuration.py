8bfc7f8fd259d790a155feff36d2dfdf4c3b0bd8
CoYe <costya.y@quali.com>
2016-07-29 17:50:16 -0700

    Removed version from driver name

--- cloudshell/networking/ericsson/ipos/ericsson_ipos_configuration.py
+++ cloudshell/networking/ericsson/ipos/ericsson_ipos_configuration.py
@@ -9,7 +9,7 @@ from cloudshell.shell.core.context_utils import get_decrypted_password_by_attrib
     get_attribute_by_name_wrapper
 from cloudshell.shell.core.dependency_injection.context_based_logger import get_logger_with_thread_id
 
-DEFAULT_PROMPT = '[>#]\s*$'
+DEFAULT_PROMPT = '\].*[>#]\s*$'
 ENABLE_PROMPT = '#\s*$'
 CONFIG_MODE_PROMPT = '\(config.*\)#\s*$'
 
@@ -34,7 +34,7 @@ DEFAULT_ACTIONS = send_default_actions
 SUPPORTED_OS = ["IP[ -]?OS"]
 AUTHENTICATION_ERROR_PATTERN = r'[Ll]ogin\s*[Ii]ncorrect|' + \
                                '[Bb]ad\s*([Pp]assword(s)?|[Ss]ecret(s)?|[Ff]ailed\s*(to)?\s*[Aa]uthenticate'
-ERROR_MAP = {'Database.*Lock.*': 'Database connection locked, try again.'}
+ERROR_MAP = {'Database.*Lock.*': 'Database locked out, please try again.'}
 
 
 def enter_enable_mode(session):
@@ -56,4 +56,4 @@ SEND_COMMAND_OPERATIONS_CLASS = EricssonSendCommandOperations
 
 GET_LOGGER_FUNCTION = get_logger_with_thread_id
 POOL_TIMEOUT = 300
-HE_MAX_LOOP_RETRIES = 2000
+HE_MAX_LOOP_RETRIES = 0
